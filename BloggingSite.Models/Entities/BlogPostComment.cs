8a240b3ad9f2a9d9151d22c55a8ccb8ba095db41
Remon <remon.onnorokom@gmail.com>
2025-04-16 11:33:47 +0600

    "add BlogPostComment Entity"

---
+++ BloggingSite.Models/Entities/BlogPostComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingSite.Models.Entities
+{
+    public class BlogPostComment
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        [ForeignKey(nameof(PostId))]
+        public ApprovedBlog? ApprovedBlog { get; set; }
+        public string Comment { get; set; }
+    }
+}
