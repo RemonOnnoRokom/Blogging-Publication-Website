8908012fd926a54ef500591a9e22945fe6dfe5b4
Remon <remon.onnorokom@gmail.com>
2025-04-16 14:42:59 +0600

    "add NotMapped on 2 properties at ApprovedBlog entities & add CreatedBy properties in PendingBlog entity"

--- BloggingSite.Models/Entities/ApprovedBlog.cs
+++ BloggingSite.Models/Entities/ApprovedBlog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,11 @@ namespace BloggingSite.Models.Entities
         public DateTime CreatedDate { get; set; }
         public int ApprovedBy { get; set; }
         public DateTime PublishedDate { get; set; }
+
+        [NotMapped]
         public List<BlogPostComment>? PostComments { get; set; }
+
+        [NotMapped]
         public List<BlogPostReaction>? Reactions { get; set; }
 
     }
--- BloggingSite.Models/Entities/PendingBlog.cs
+++ BloggingSite.Models/Entities/PendingBlog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,11 @@ namespace BloggingSite.Models.Entities
     public class PendingBlog
     {
         public int Id { get; set; }
+
+        [Required]
         public string Content { get; set; }
+        public string CreatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
+        
     }
 }
