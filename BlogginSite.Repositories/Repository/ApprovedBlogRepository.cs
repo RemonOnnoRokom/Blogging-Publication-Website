bfd42e47cdcb317485eaa276214054564903a5fb
Remon <remon.onnorokom@gmail.com>
2025-04-16 15:52:52 +0600

    "Implement the IApprovedBlogRepository"

---
+++ BlogginSite.Repositories/Repository/ApprovedBlogRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloggingSite.Models.Entities;
+using BlogginSite.Repositories.Db;
+using BlogginSite.Repositories.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BlogginSite.Repositories.Repository
+{
+    public class ApprovedBlogRepository : IApprovedBlogRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ApprovedBlogRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(ApprovedBlog entity)
+        {
+             _context.ApprovedBlogs.Add(entity);
+            await _context.SaveChangesAsync();
+           
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var obj = await GetByIdAsync(id);
+             _context.ApprovedBlogs.Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ApprovedBlog>> GetAllAsync()
+        {
+            return await _context.ApprovedBlogs.ToListAsync();
+        }
+
+        public async Task<ApprovedBlog> GetByIdAsync(int id)
+        {
+            return await _context.ApprovedBlogs.Where(x=>x.Id == id).FirstAsync();
+        }
+
+        public void Update(ApprovedBlog entity)
+        {
+            _context.ApprovedBlogs.Update(entity);
+        }
+    }
+}
