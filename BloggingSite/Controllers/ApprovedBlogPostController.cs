0cb23d3685616e9efc4786930e72c67b4f954b2b
Remon <remon.onnorokom@gmail.com>
2025-04-16 12:29:14 +0600

    "Add Index action method"

---
+++ BloggingSite/Controllers/ApprovedBlogPostController.cs
@@ -0,0 +1,29 @@
+﻿using BlogginSite.Repositories.Db;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingSite.Controllers
+{
+    public class ApprovedBlogPostController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ApprovedBlogPostController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.PendingBlogs.ToList();
+
+            return View(list);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var list = _context.PendingBlogs.Where(x => x.Id == id).FirstOrDefault();
+            _context.PendingBlogs.Remove(list);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
