5fc8d777d701cf606e1542c80219027662b5a9ab
Remon <remon.onnorokom@gmail.com>
2025-04-16 11:22:09 +0600

    "Rename Reaction(enum) to Expression(enum)"

---
+++ BloggingSite.Models/Entities/Expression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingSite.Models.Entities
+{
+    public enum Reaction
+    {
+        Like = 1,
+        Dislike
+    }
+}
