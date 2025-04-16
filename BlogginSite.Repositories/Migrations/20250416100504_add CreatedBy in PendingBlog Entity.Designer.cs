aa2a091f627089237af80ec952817bd4e5e2aa3d
Remon <remon.onnorokom@gmail.com>
2025-04-16 16:08:20 +0600

    "add 1 extra property in PendingBlog entity and add migration and add a navbar in _Layout page for navigation ApprovedBlogPostController"

--- BlogginSite.Repositories/Migrations/20250416100504_add CreatedBy in PendingBlog Entity.Designer.cs
+++ BlogginSite.Repositories/Migrations/20250416100504_add CreatedBy in PendingBlog Entity.Designer.cs
@@ -4,6 +4,7 @@ using BlogginSite.Repositories.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogginSite.Repositories.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250416100504_add CreatedBy in PendingBlog Entity")]
+    partial class addCreatedByinPendingBlogEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
