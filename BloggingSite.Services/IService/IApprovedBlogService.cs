fad4fae2841e50c3f3e0826a382dbe9fc35cc633
Remon <remon.onnorokom@gmail.com>
2025-04-16 16:29:47 +0600

    "Create IApprovedBlogService in Services project"

---
+++ BloggingSite.Services/IService/IApprovedBlogService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloggingSite.Models.Entities;
+
+namespace BloggingSite.Services.IService
+{
+    public interface IApprovedBlogService
+    {
+        Task<IEnumerable<ApprovedBlog>> GetAllAsync();
+        Task<ApprovedBlog> GetByIdAsync(int id);
+        Task AddAsync(ApprovedBlog entity);
+        void Update(ApprovedBlog entity);
+        Task DeleteAsync(int id);
+    }
+}
