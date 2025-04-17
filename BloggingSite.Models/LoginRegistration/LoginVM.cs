03fe32b4599f8c28af07ac2c8c260ddf3fbf38c4
Remon <remon.onnorokom@gmail.com>
2025-04-17 17:28:41 +0600

    "Add LoginVM model for Login"

---
+++ BloggingSite.Models/LoginRegistration/LoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingSite.Models.LoginRegistration
+{
+    public class LoginVM
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; } = false;
+    }
+}
