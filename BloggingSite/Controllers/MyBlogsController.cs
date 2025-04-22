22c85a269870a3b500d5fb23ebe5a147721a84b5
Remon <remon.onnorokom@gmail.com>
2025-04-22 16:02:42 +0600

    "Didn't follow the Task-Async Prog style.That's why InvalidOperationException got.Now,I solved!"

---
+++ BloggingSite/Controllers/MyBlogsController.cs
@@ -0,0 +1,38 @@
+﻿using BloggingSite.Models.Entities;
+using BloggingSite.Models.ViewModel;
+using BlogginSite.Repositories.Db;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggingSite.Controllers
+{
+    [Authorize]
+    public class MyBlogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<MyUser> _userManager;
+        public MyBlogsController(ApplicationDbContext context , UserManager<MyUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            ApprovedBlogVM approvedBlogVM = new ApprovedBlogVM();
+            long id = ( await _userManager.FindByNameAsync(User.Identity.Name)).Id;
+
+            approvedBlogVM.ApprovedBlogs = _context.ApprovedBlogs.Where(x=>x.CreatedBy == id).Take(6).ToList();
+            approvedBlogVM.ItemNumber = 5;
+
+            return View(approvedBlogVM);
+        }
+
+        public IActionResult LoadMore(int skip)
+        {
+
+
+            return View();
+        }
+    }
+}
