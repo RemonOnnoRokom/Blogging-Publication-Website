7a95726011c0a83402f8387994f798ae3663cce8
Remon <remon.onnorokom@gmail.com>
2025-04-23 11:44:35 +0600

    "add a ViewModel for Change Password"

---
+++ BloggingSite.Models/ViewModel/ChangePasswordVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloggingSite.Models.ViewModel
+{
+    public class ChangePasswordVM
+    {
+        [Display(Name ="Current Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="New Password")]
+        public string NewPassword { get; set; }
+    }
+}
