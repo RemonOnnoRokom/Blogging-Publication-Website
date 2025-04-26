2d1f9e15c87b79a28955d8f30d0ab8d6b6deaac5
Remon <remon.onnorokom@gmail.com>
2025-04-26 13:53:27 +0600

    "made this model to collect data[PostId,AdminStatus] as a model."

---
+++ BloggingSite.Models/ViewModel/AdminApprovedVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloggingSite.Models.Entities;
+
+namespace BloggingSite.Models.ViewModel
+{
+    public class AdminApprovedVM
+    {
+        public int PostId { get; set; }
+        public long AdminId { get; set; }
+        public BlogStatus AdminStatus { get; set; }
+    }
+}
