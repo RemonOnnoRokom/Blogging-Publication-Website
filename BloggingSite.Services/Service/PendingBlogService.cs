319cc0a034f418a3cd930a906f6cbc698a9adbbd
Remon <remon.onnorokom@gmail.com>
2025-04-27 16:14:09 +0600

    "changes needed according to repository layer."

--- BloggingSite.Services/Service/PendingBlogService.cs
+++ BloggingSite.Services/Service/PendingBlogService.cs
@@ -17,36 +17,12 @@ namespace BloggingSite.Services.Service
         {
             _repository = repository;
         }
-        public async Task AddAsync(PendingBlog entity)
-        {
-            ApprovedBlog Obj = new ApprovedBlog();
-
-            Obj.CreatedBy = entity.CreatedBy;
-            Obj.Content = entity.Content;
-            Obj.CreatedDate = entity.CreatedDate;
-            
-            await _repository.AddAsync(Obj);                            
-        }
-
-        public void Approved(AdminApprovedVM obj)
-        {            
-            var dbObj = _repository.GetByIdAsync(obj.PostId);
-            dbObj.CurrentStatus = obj.AdminStatus;
-            dbObj.ApprovedBy = obj.AdminId;
-
-            _repository.Update(dbObj);
-        }
-
-        public async Task DeleteAsync(int id)
-        {
-            await _repository.DeleteAsync(id);
-        }
 
         public async Task<IEnumerable<PendingBlog>> GetAllAsync()
         {
-            List<ApprovedBlog> list =  await _repository.GetAllAsync();
+            List<ApprovedBlog> list = await _repository.GetAllAsync();
 
-            List<PendingBlog> result = list.Where(x=>x.CurrentStatus == BlogStatus.Create)
+            List<PendingBlog> result = list.Where(x => x.CurrentStatus == BlogStatus.Create)
                                             .Select(x =>
                                                         new PendingBlog()
                                                         {
@@ -55,17 +31,17 @@ namespace BloggingSite.Services.Service
                                                             Content = x.Content,
                                                             CreatedDate = x.CreatedDate
                                                         })
-                                            .ToList(); 
+                                            .ToList();
 
             return result;
         }
 
-        public PendingBlog GetByIdAsync(int id)
+        public async Task<PendingBlog> GetByIdAsync(int id)
         {
-            ApprovedBlog Obj =  _repository.GetByIdAsync(id);
+            ApprovedBlog Obj = await _repository.GetByIdAsync(id);
 
             PendingBlog result = null;
-            if(Obj.CurrentStatus == BlogStatus.Create)
+            if (Obj.CurrentStatus == BlogStatus.Create)
             {
                 result = new PendingBlog()
                 {
@@ -79,9 +55,34 @@ namespace BloggingSite.Services.Service
             return result;
         }
 
-        public void Update(ApprovedBlog entity)
+        public async Task AddAsync(PendingBlog entity)
         {
-             _repository.Update(entity);
-        }       
+            ApprovedBlog Obj = new ApprovedBlog();
+
+            Obj.CreatedBy = entity.CreatedBy;
+            Obj.Content = entity.Content;
+            Obj.CreatedDate = entity.CreatedDate;
+            
+            await _repository.AddAsync(Obj);                            
+        }
+
+        public async Task UpdateAsync(ApprovedBlog obj)
+        {
+           await _repository.UpdateAsync(obj);
+        }
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+        }
+
+        public async Task ApprovedAsync(AdminApprovedVM obj)
+        {
+            var dbObj = await _repository.GetByIdAsync(obj.PostId);
+            dbObj.CurrentStatus = obj.AdminStatus;
+            dbObj.ApprovedBy = obj.AdminId;
+
+            await _repository.UpdateAsync(dbObj);
+        }
+                          
     }
 }
