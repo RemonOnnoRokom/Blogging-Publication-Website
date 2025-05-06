d88093d92699916a16e34f464767f5b2dbbdda52
Remon <remon.onnorokom@gmail.com>
2025-05-06 12:34:51 +0600

    "Create an class.An paste the GetByIdAsync service test method."

---
+++ Blogging.Tests/Services/PendingBlogServiceTest/PendingBlogServiceGetByIdAsyncTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BloggingSite.Models.Entities;
+using NSubstitute;
+
+namespace Blogging.Tests.Services.PendingBlogServiceTest
+{
+    public class PendingBlogServiceGetByIdAsyncTest : PendingBlogServiceBeseTest
+    {
+        public PendingBlogServiceGetByIdAsyncTest()
+        {
+            
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_RepoReturnApprovedBlog_ReturnApprovedBlog()
+        {
+            //Arrange 
+            const int id = 1;
+            var expectedApprovedBlog = GetByIdDummyData();
+
+            _approvedBlogRepository.GetByIdAsync(id).Returns(expectedApprovedBlog);
+
+            //Act
+            var result = await _sut.GetByIdAsync(id);
+
+            //Assert
+            Assert.Equal(expectedApprovedBlog.Id, result.Id);
+            Assert.Equal(expectedApprovedBlog.Content, result.Content);
+            Assert.Equal(expectedApprovedBlog.CreatedDate, result.CreatedDate);
+        }
+
+        #region helper(Specific Id)
+        public ApprovedBlog GetByIdDummyData()
+        {
+            var specificBlog = new ApprovedBlog()
+            {
+                Id = 1,
+                CreatedBy = 10,
+                Content = "Humpti dumpti drum drum",
+                CreatedDate = DateTime.Now
+
+            };
+            return specificBlog;
+        }
+        #endregion
+    }
+}
