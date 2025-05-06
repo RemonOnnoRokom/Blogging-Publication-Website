dc59e6cbae769c605aa0eb67f46d720a925ef9b7
Remon <remon.onnorokom@gmail.com>
2025-05-06 15:27:08 +0600

    "Pasting UpdateAsync,DeleteAsync,ApprovedAsync test method in this file."

--- Blogging.Tests/Services/PendingBlogServiceTest/PendingBlogServiceOerationalFunctionTest.cs
+++ Blogging.Tests/Services/PendingBlogServiceTest/PendingBlogServiceOerationalFunctionTest.cs
@@ -29,6 +29,77 @@ namespace Blogging.Tests.Services.PendingBlogServiceTest
             await _approvedBlogRepository.Received(1).AddAsync(Arg.Any<ApprovedBlog>());
         }
 
+        [Fact]
+        public async Task UpdateAsync_ValidEntity_UpdateSuccessful()
+        {
+            //Arrange
+            var GetByIdSpecificData = GetByIdDummyData();
+
+            //Act
+            await _sut.UpdateAsync(GetByIdSpecificData);
+
+            //Assert
+            await _approvedBlogRepository.Received(1).UpdateAsync(GetByIdSpecificData);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ValidEntity_DeleteSuccessful()
+        {
+            //Arrange
+            const int id = 2;
+
+            //Act
+            await _sut.DeleteAsync(id);
+
+            //Assert
+            await _approvedBlogRepository.Received(1).DeleteAsync(Arg.Any<int>());
+        }
+
+        [Fact]
+        public async Task ApprovedAsync_BlogStatusApproved_BlogApprovedSuccessful()
+        {
+            //Arrange
+            var entityData = DummyAdminApprovedVM();
+            var entityGetById = GetByIdDummyData();
+
+            _approvedBlogRepository.GetByIdAsync(entityData.PostId).Returns(entityGetById);
+
+            //Act
+            await _sut.ApprovedAsync(entityData);
+
+            //Assert
+            await _approvedBlogRepository.Received(1).UpdateAsync(Arg.Any<ApprovedBlog>());
+        }
+
+        #region helper(Specific Id)
+        public ApprovedBlog GetByIdDummyData()
+        {
+            var specificBlog = new ApprovedBlog()
+            {
+                Id = 1,
+                CreatedBy = 10,
+                Content = "Humpti dumpti drum drum",
+                CreatedDate = DateTime.Now
+
+            };
+            return specificBlog;
+        }
+        #endregion        
+
+        #region helper(AdminApprovedVm)
+        public AdminApprovedVM DummyAdminApprovedVM()
+        {
+            AdminApprovedVM entity = new AdminApprovedVM()
+            {
+                PostId = 3,
+                AdminId = 0,
+                AdminStatus = BlogStatus.Approved
+            };
+
+            return entity;
+        }
+        #endregion         
+
         #region helper(AddAsync)
         public PendingBlog GetPendingBlog()
         {
