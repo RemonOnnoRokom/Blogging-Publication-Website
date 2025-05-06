90cb0b28f55f714e971bc43d5860fd2763acfaa8
Remon <remon.onnorokom@gmail.com>
2025-05-06 12:15:45 +0600

    "add common field and constructor."

--- Blogging.Tests/Services/PendingBlogServiceTest/PendingBlogServiceBeseTest.cs
+++ Blogging.Tests/Services/PendingBlogServiceTest/PendingBlogServiceBeseTest.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BloggingSite.Services.IService;
+using BloggingSite.Services.Service;
+using BlogginSite.Repositories.IRepository;
+using NSubstitute;
 
 namespace Blogging.Tests.Services.PendingBlogServiceTest
 {
     public class PendingBlogServiceBeseTest
     {
 
+        private readonly IPendingBlogService _sut;
+        private readonly IApprovedBlogRepository _approvedBlogRepository;
+
+        public PendingBlogServiceBeseTest()
+        {
+            _approvedBlogRepository = Substitute.For<IApprovedBlogRepository>();
+            _sut = new PendingBlogService(_approvedBlogRepository);
+        }
     }
 }
