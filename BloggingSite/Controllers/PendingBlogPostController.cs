d9da397fa6a51e5a0f997cdd31c8793ffaaeeac5
Remon <remon.onnorokom@gmail.com>
2025-05-07 17:49:38 +0600

    "Add try/catch block and modify after reviewing by Ahsan vai."

--- BloggingSite/Controllers/PendingBlogPostController.cs
+++ BloggingSite/Controllers/PendingBlogPostController.cs
@@ -23,17 +23,17 @@ namespace BloggingSite.Controllers
 
         public async Task<IActionResult> Index()
         {
+            IEnumerable<PendingBlog> list = new List<PendingBlog>();
             try
             {
-                var list = await _service.GetAllAsync();
-
-                return View(list);
+                list = await _service.GetAllAsync();
             }
             catch(Exception)
             {
-                return View("404NotFound");
+                ViewData["Error"] = "Something Error occurs";
             }
-            
+
+            return View(list);
         }
 
         public async Task<IActionResult> Delete(int id)
@@ -41,28 +41,28 @@ namespace BloggingSite.Controllers
             try
             {
                 await _service.DeleteAsync(id);
-
-                return RedirectToAction("Index");
             }
             catch(Exception)
             {
-                return View("404NotFound");
+                TempData["Error"] = "Not working Properly";                
             }
             
+            return RedirectToAction("Index");
         }
        
-        public IActionResult SeeMore(int id)
+        public async Task<IActionResult> SeeMore(int id)
         {
+            PendingBlog blog = new PendingBlog();
             try
             {
-                var obj = _service.GetByIdAsync(id);
-
-                return View(nameof(Approved), obj);
+                blog = await _service.GetByIdAsync(id);                
             }
             catch (Exception)
             {
-                return View("404NotFound");
+                ViewData["Error"] = "Not Found";
             }
+
+            return View(nameof(Approved), blog);
         }
         
         public async Task<IActionResult> Approved([Bind("PostId,AdminStatus")]AdminApprovedVM obj)
@@ -71,49 +71,47 @@ namespace BloggingSite.Controllers
             {
                 MyUser adminUser = await _userManager.FindByNameAsync(User.Identity.Name);
                 obj.AdminId = adminUser.Id;
-                await _service.ApprovedAsync(obj);
-
-                return RedirectToAction(nameof(Index));
+                await _service.ApprovedAsync(obj);               
             }
             catch (Exception)
             {
-                return View("404NotFound");
+                TempData["Error"] = "Not Approved";                
             }
 
+            return RedirectToAction(nameof(Index));
         }
 
         public async Task<IActionResult> Top5Blogs([FromServices] ApplicationDbContext context)
         {
+            List<TopBlogVM> topBlogEntities = new List<TopBlogVM>();
             try
             {
                 int skipBlogs = 0;
                 int numberOfTopBlog = 5;
 
-                var ids = await context.Database.SqlQuery<TopBlogVM>($"select  [a].[Id] , [a].[Content]  \r\n from((select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs inner join  PostComments on ApprovedBlogs.Id =PostComments.PostId) \r\n union all\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\tinner join  PostReactions\r\n\t\t\ton ApprovedBlogs.Id =PostReactions.PostId\r\n\t\t)\r\n) \r\n\t\tas a group by a.Id , a.Content  order by count(a.Id) desc;select  [a].[Id] , [a].[Content]  \r\nfrom(\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\t\t\tinner join  PostComments\r\n\t\t\t\t\ton ApprovedBlogs.Id =PostComments.PostId) \r\nunion all\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\tinner join  PostReactions\r\n\t\t\ton ApprovedBlogs.Id =PostReactions.PostId\r\n\t\t)\r\n) \r\n\t\tas a group by a.Id , a.Content  order by count(a.Id) desc OFFSET {skipBlogs} ROWs \r\n\tFETCH Next  {numberOfTopBlog - 1} rows only").ToListAsync();
-
-                return View(ids);
+                topBlogEntities = await context.Database.SqlQuery<TopBlogVM>($"select  [a].[Id] , [a].[Content]  \r\n from((select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs inner join  PostComments on ApprovedBlogs.Id =PostComments.PostId) \r\n union all\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\tinner join  PostReactions\r\n\t\t\ton ApprovedBlogs.Id =PostReactions.PostId\r\n\t\t)\r\n) \r\n\t\tas a group by a.Id , a.Content  order by count(a.Id) desc;select  [a].[Id] , [a].[Content]  \r\nfrom(\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\t\t\tinner join  PostComments\r\n\t\t\t\t\ton ApprovedBlogs.Id =PostComments.PostId) \r\nunion all\r\n\t\t(select ApprovedBlogs.Id , ApprovedBlogs.Content   From ApprovedBlogs \r\n\t\t\tinner join  PostReactions\r\n\t\t\ton ApprovedBlogs.Id =PostReactions.PostId\r\n\t\t)\r\n) \r\n\t\tas a group by a.Id , a.Content  order by count(a.Id) desc OFFSET {skipBlogs} ROWs \r\n\tFETCH Next  {numberOfTopBlog - 1} rows only").ToListAsync();     
             }
             catch (Exception)
             {
-                return View("404NotFound");
+                ViewData["Error"] = "Not working Properly due to the Error";              
             }
 
+            return View(topBlogEntities);
         }
 
         public async Task<IActionResult> UserList([FromServices]ApplicationDbContext context)
         {
+            List<MyUser> list = new List<MyUser>();
             try
-            {
-                List<MyUser> list;
+            {               
                 list = context.Users.Where(x => context.UserRoles.Any(y => y.UserId == x.Id) == false).ToList();
-
-                return View(list);
             }
             catch (Exception)
             {
-                return View("404NotFound");
+                ViewData["Error"] = "Not working Properly due to the Error";
             }
 
+            return View(list);
         }
 
         public async Task<IActionResult> Permission(int id)
@@ -130,14 +128,14 @@ namespace BloggingSite.Controllers
                     obj.LockoutEnd = null;
                 }
 
-                IdentityResult result = await _userManager.UpdateAsync(obj);
-
-                return RedirectToAction(nameof(UserList));
+                IdentityResult result = await _userManager.UpdateAsync(obj);  
             }
             catch (Exception)
             {
-                return View("404NotFound");
+                TempData["Error"] = "Not working Properly";
             }
+
+            return RedirectToAction(nameof(UserList));
         }
     }
 }
